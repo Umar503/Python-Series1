db1316786bbe274ab3613f583f04f0451825bf48
Umar503 <umararshad563@gmail.com>
2024-01-24 13:17:03 +0500

    pythoon

---
+++ Calculater/calculater.py
@@ -0,0 +1,14 @@
+a = 15
+b = 5
+c = a + b
+print("Sum of", a, "and", b, "is", c)
+c = a/b
+print("Division of", a, "and", b, "is", c)
+c = a*b
+print("Multiply of", a, "and", b, "is", c)
+c = a**b
+print("Power of", a, "", b, "is", c)
+c = a%b
+print("Modules of", a, "and", b, "is", c)
+c = a//b
+print("Divison of", a, "and", b, "is", c)
